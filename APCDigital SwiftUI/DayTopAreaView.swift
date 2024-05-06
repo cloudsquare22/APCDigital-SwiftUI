5319c65d700e826afe019a9824f45a44eea5973d
shi-n <shi-n_xyz@cloudsquare.jp>
2024-05-06 20:17:08 +0900

    構築中

--- APCDigital SwiftUI/DayTopAreaView.swift
+++ APCDigital SwiftUI/DayTopAreaView.swift
@@ -11,20 +11,8 @@ struct DayTopAreaView: View {
     @Environment(DateManagement.self) private var dateManagement
     
     var body: some View {
-        ForEach(WeekDay1stMonday.allCases, id: \.self) { weekday in
-            if let day = self.dateManagement.days[weekday] {
-                Text("\(day)")
-                    .foregroundStyle(Color("Basic Green", bundle: .main))
-                    .font(Font.system(size: 24.0, weight: .semibold))
-                    .offset(x: self.dateManagement.getDayLebelPosition(weekDay: weekday).x,
-                            y: self.dateManagement.getDayLebelPosition(weekDay: weekday).y)
-                Text("\(self.dateManagement.getRemainingLebel(weekDay: weekday).contents)")
-                    .foregroundStyle(Color("Basic Green", bundle: .main))
-                    .font(Font.system(size: 10.0, weight: .semibold))
-                    .offset(x: self.dateManagement.getRemainingLebel(weekDay: weekday).x,
-                            y: self.dateManagement.getRemainingLebel(weekDay: weekday).y)
-            }
-        }
+        DayLabelView()
+        RemainingLebelView()
         HolidayLabelView()
         EventsAllDayView()
     }
