91ab1b89e3408a3e6d3521d9e30101b03b7ce0a0
shi-n <shi-n_xyz@cloudsquare.jp>
2024-05-06 22:26:35 +0900

    構築中

---
+++ APCDigital SwiftUI/WeekOfYearView.swift
@@ -0,0 +1,18 @@
+//
+//  WeekOfYearView.swift
+//  APCDigital SwiftUI
+//
+//  Created by Shin Inaba on 2024/05/06.
+//
+
+import SwiftUI
+
+struct WeekOfYearView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WeekOfYearView()
+}
