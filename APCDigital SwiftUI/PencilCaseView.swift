20439d776187330f32866ab2338b5a26f93bf702
shi-n <shi-n_xyz@cloudsquare.jp>
2024-05-07 22:00:00 +0900

    構築中

---
+++ APCDigital SwiftUI/PencilCaseView.swift
@@ -0,0 +1,18 @@
+//
+//  PencilCaseView.swift
+//  APCDigital SwiftUI
+//
+//  Created by Shin Inaba on 2024/05/07.
+//
+
+import SwiftUI
+
+struct PencilCaseView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PencilCaseView()
+}
