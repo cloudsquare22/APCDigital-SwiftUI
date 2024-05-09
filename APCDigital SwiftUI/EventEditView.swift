305109930e490261cb1468562077d2b4d18e821e
shi-n <shi-n_xyz@cloudsquare.jp>
2024-05-09 21:55:37 +0900

    構築中

---
+++ APCDigital SwiftUI/EventEditView.swift
@@ -0,0 +1,18 @@
+//
+//  EventEditView.swift
+//  APCDigital SwiftUI
+//
+//  Created by Shin Inaba on 2024/05/09.
+//
+
+import SwiftUI
+
+struct EventEditView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EventEditView()
+}
