e318af56a746c1e31a485d92ee46250b74b90e74
shi-n <shi-n_xyz@cloudsquare.jp>
2024-05-15 22:15:22 +0900

    構築中

--- APCDigital SwiftUI/Device.swift
+++ APCDigital SwiftUI/Device.swift
@@ -12,15 +12,21 @@ class Device {
     enum DType {
         case ipad_pro_12_9_6th
         case ipad_pro_12_9_6th_more_space
+        case ipad_pro_13
+        case ipad_pro_13_more_space
         case etc
     }
     static func getDevie() -> DType {
 //        print("Device:\(UIScreen.main.bounds.size)")
         var dtype: DType = .etc
         switch UIScreen.main.bounds.size.width {
+        case 1600.0:
+            dtype = .ipad_pro_13_more_space
         case 1590.0:
 //            print("iPad Pro 12.9 第6世代 スペースを拡大")
             dtype = .ipad_pro_12_9_6th_more_space
+        case 1376.0:
+            dtype = .ipad_pro_13
         case 1366.0:
 //            print("iPad Pro 12.9 第6世代 デフォルト")
             dtype = .ipad_pro_12_9_6th
