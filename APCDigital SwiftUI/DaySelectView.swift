290104d415d2847d48e124dd0217bd27fb2fc4ff
shi-n <shi-n_xyz@cloudsquare.jp>
2024-05-17 21:42:16 +0900

    構築中

---
+++ APCDigital SwiftUI/DaySelectView.swift
@@ -0,0 +1,18 @@
+//
+//  DaySelectView.swift
+//  APCDigital SwiftUI
+//
+//  Created by Shin Inaba on 2024/05/16.
+//
+
+import SwiftUI
+
+struct DaySelectView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DaySelectView()
+}
