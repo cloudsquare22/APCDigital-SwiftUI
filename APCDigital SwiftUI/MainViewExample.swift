e41e4f53b8457a8a9ff9fbc9d1cbf8720abe5140
shi-n <shi-n_xyz@cloudsquare.jp>
2024-10-25 22:15:01 +0900

    切り分け用コード追加

---
+++ APCDigital SwiftUI/MainViewExample.swift
@@ -0,0 +1,18 @@
+//
+//  MainViewExample.swift
+//  APCDigital SwiftUI
+//
+//  Created by Shin Inaba on 2024/10/25.
+//
+
+import SwiftUI
+
+struct MainViewExample: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainViewExample()
+}
---
+++ APCDigital SwiftUI/PencilKitViewRepresentableExample.swift
@@ -0,0 +1,8 @@
+//
+//  PencilKitViewRepresentableExample.swift
+//  APCDigital SwiftUI
+//
+//  Created by Shin Inaba on 2024/10/25.
+//
+
+import Foundation
