b033ee5bd393de7c2feb6b5f9a0e1e7fd8bfe5b7
shi-n <shi-n_xyz@cloudsquare.jp>
2025-10-17 22:18:57 +0900

    MenuView追加

---
+++ APCDigital SwiftUI/MenuView.swift
@@ -0,0 +1,18 @@
+//
+//  MenuView.swift
+//  APCDigital SwiftUI
+//
+//  Created by Shin Inaba on 2025/10/17.
+//
+
+import SwiftUI
+
+struct MenuView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MenuView()
+}
