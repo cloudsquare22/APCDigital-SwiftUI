d4d17df059da8768fb03fb94008b6dc370f94666
shi-n <shi-n_xyz@cloudsquare.jp>
2025-10-18 14:41:15 +0900

    EventListView追加

---
+++ APCDigital SwiftUI/EventListView.swift
@@ -0,0 +1,18 @@
+//
+//  EventListView.swift
+//  APCDigital SwiftUI
+//
+//  Created by Shin Inaba on 2025/10/18.
+//
+
+import SwiftUI
+
+struct EventListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EventListView()
+}
