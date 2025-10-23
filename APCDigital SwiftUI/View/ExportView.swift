595ed94f16ba183fabe57bd05b9566dbb22e8479
shi-n <shi-n_xyz@cloudsquare.jp>
2025-10-24 06:42:23 +0900

    ExportView追加

---
+++ APCDigital SwiftUI/View/ExportView.swift
@@ -0,0 +1,18 @@
+//
+//  ExportView.swift
+//  APCDigital SwiftUI
+//
+//  Created by Shin Inaba on 2025/10/23.
+//
+
+import SwiftUI
+
+struct ExportView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExportView()
+}
