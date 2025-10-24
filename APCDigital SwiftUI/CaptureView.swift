77af95fdf552c6c4e09dd6c17422957a74821ac4
shi-n <shi-n_xyz@cloudsquare.jp>
2025-10-25 07:57:25 +0900

    エクスポート用キャプチャView作成（重複、不格好だけど、共通化出来ない）

---
+++ APCDigital SwiftUI/CaptureView.swift
@@ -0,0 +1,18 @@
+//
+//  CaptureView.swift
+//  APCDigital SwiftUI
+//
+//  Created by Shin Inaba on 2025/10/24.
+//
+
+import SwiftUI
+
+struct CaptureView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CaptureView()
+}
