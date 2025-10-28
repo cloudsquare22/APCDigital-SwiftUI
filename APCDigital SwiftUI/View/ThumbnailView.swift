e82b0767491d7f95f1ad46441b625eb57b881451
shi-n <shi-n_xyz@cloudsquare.jp>
2025-10-28 22:15:39 +0900

    PaperMarkupのサムネイル表示は出来るが、Capture側に組み込むのは難しい、asyncが

---
+++ APCDigital SwiftUI/View/ThumbnailView.swift
@@ -0,0 +1,18 @@
+//
+//  ThumbnailView.swift
+//  APCDigital SwiftUI
+//
+//  Created by Shin Inaba on 2025/10/28.
+//
+
+import SwiftUI
+
+struct ThumbnailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ThumbnailView()
+}
