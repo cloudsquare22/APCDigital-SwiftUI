05f835aa9c5bc0b70e47789ecb6a86ddc5c9ea47
shi-n <shi-n_xyz@cloudsquare.jp>
2025-12-25 22:12:13 +0900

    AboutView：閉じる処理追加

--- APCDigital SwiftUI/View/AboutView.swift
+++ APCDigital SwiftUI/View/AboutView.swift
@@ -8,8 +8,22 @@
 import SwiftUI
 
 struct AboutView: View {
+    @Environment(\.dismiss) var dismiss
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                .navigationTitle("About")
+                .navigationBarTitleDisplayMode(.inline)
+                .toolbar(content: {
+                    ToolbarItem(placement: .cancellationAction, content: {
+                        Button("Cancel",
+                               action: {
+                            dismiss()
+                        })
+                    })
+                })
+        }
     }
 }
 
