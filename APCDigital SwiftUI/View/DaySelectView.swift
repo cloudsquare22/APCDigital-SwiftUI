4b72d8377fab82f5221cd962173e576abb970c6f
shi-n <shi-n_xyz@cloudsquare.jp>
2025-12-26 22:08:57 +0900

    DaySelectViewの他デザインと統一

--- APCDigital SwiftUI/View/DaySelectView.swift
+++ APCDigital SwiftUI/View/DaySelectView.swift
@@ -9,52 +9,41 @@ import SwiftUI
 
 struct DaySelectView: View {
     @State var selectDay: Date = Date()
-    @Environment(\.presentationMode) var presentationMode
+    @Environment(\.dismiss) var dismiss
     @Environment(DateManagement.self) private var dateManagement
     @Environment(EventManagement.self) private var eventMangement
 
     var body: some View {
-        VStack {
-            Text("Select Day")
-            DatePicker("",
-                       selection: self.$selectDay,
-                       displayedComponents: [.date])
-                .datePickerStyle(GraphicalDatePickerStyle())
-                .environment(\.calendar, Calendar(identifier: .gregorian).withMondayAsFirstDayOfWeek())
-                .frame(width: 320)
-            HStack {
-                Spacer()
-                Button(action: {
-                    self.dateManagement.setPageStartday(direction: .today, selectday: self.selectDay)
-                    self.eventMangement.updateEvents(startDay: self.dateManagement.daysDateComponents[.monday]!,
-                                                     endDay: self.dateManagement.daysDateComponents[.sunday]!)
-                    self.eventMangement.updateCalendars()
-                    self.presentationMode.wrappedValue.dismiss()
-                }, label: {
-                    Text("Select")
-                })
-                .padding(8)
-                .frame(width: 100)
-                .overlay {
-                    RoundedRectangle(cornerRadius: 8)
-                        .stroke(Color.blue, lineWidth: 1)
-                }
-                Spacer()
-                Button(action: {
-                    self.presentationMode.wrappedValue.dismiss()
-                }, label: {
-                    Text("Close")
-                })
-                .padding(8)
-                .frame(width: 100)
-                .overlay {
-                    RoundedRectangle(cornerRadius: 8)
-                        .stroke(Color.blue, lineWidth: 1)
-                }
-                Spacer()
+        NavigationStack {
+            VStack {
+                DatePicker("",
+                           selection: self.$selectDay,
+                           displayedComponents: [.date])
+                    .datePickerStyle(GraphicalDatePickerStyle())
+                    .environment(\.calendar, Calendar(identifier: .gregorian).withMondayAsFirstDayOfWeek())
+                    .frame(width: 320)
             }
+            .navigationTitle("Select Day")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar(content: {
+                ToolbarItem(placement: .cancellationAction, content: {
+                    Button("Cancel",
+                           action: {
+                        dismiss()
+                    })
+                })
+                ToolbarItem(placement: .confirmationAction, content: {
+                    Button("Select",
+                           action: {
+                        self.dateManagement.setPageStartday(direction: .today, selectday: self.selectDay)
+                        self.eventMangement.updateEvents(startDay: self.dateManagement.daysDateComponents[.monday]!,
+                                                         endDay: self.dateManagement.daysDateComponents[.sunday]!)
+                        self.eventMangement.updateCalendars()
+                        dismiss()
+                    })
+                })
+            })
         }
-        .padding(8)
         .onAppear() {
             if let pagestartday = self.dateManagement.pagestartday {
                 self.selectDay = pagestartday
