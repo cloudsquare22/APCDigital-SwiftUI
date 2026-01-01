2d447cb18a529b0c47989b66a2a95e116d317660
shi-n <shi-n_xyz@cloudsquare.jp>
2026-01-01 10:03:23 +0900

    公開用に向けてSettingVIew構築開始

---
+++ APCDigital SwiftUI/View/SettingView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingView.swift
+//  APCDigital SwiftUI
+//
+//  Created by Shin Inaba on 2026/01/01.
+//
+
+import SwiftUI
+
+struct SettingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingView()
+}
