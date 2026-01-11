9ef431f94d1f7c768b5124fb1a98b0843f6a920c
shi-n <shi-n_xyz@cloudsquare.jp>
2026-01-11 21:43:39 +0900

    CalendarSelectView追加と遷移

---
+++ APCDigital SwiftUI/View/CalendarSelectView.swift
@@ -0,0 +1,18 @@
+//
+//  CalendarSelectView.swift
+//  APCDigital SwiftUI
+//
+//  Created by Shin Inaba on 2026/01/11.
+//
+
+import SwiftUI
+
+struct CalendarSelectView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CalendarSelectView()
+}
